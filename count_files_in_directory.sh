8d6d68caba70ba66019e2b287203176b56a66ec8
Julián Linares <64713456+bixis@users.noreply.github.com>
2023-01-20 20:21:08 -0300

    exercise 2 completed

---
+++ count_files_in_directory.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# This script counts the files inside the specified directory.
+
+function usage {
+	echo "./$(basename $0) -h --> shows usage"
+}
+
+# This are the flags expected in the input
+optstring=":t:h"
+
+#This handles the flags and inputs
+while getopts $optstring options; do
+	case $options in
+		h) 	
+			usage;;
+		t) 	
+			TARGET_DIR=$OPTARG;;
+		:) 
+			echo "Must supply the directory path in which you want to count the files"
+			echo "For example:"
+		   	echo "$basename $0 t ../exampleDir"
+		   	exit 1;;
+	   	?)
+			echo "Invalid option -${PTARG}"
+			exit 2;;
+
+	esac
+done
+
+echo `ls ${TARGET_DIR} | wc -l`
+exit 0
+
